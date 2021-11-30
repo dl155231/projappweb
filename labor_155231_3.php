763eaf52abd92ab87bc4f162242c8a7f40820abf
s155231 <155231@student.uwm.edu.pl>
2021-11-30 20:31:28 +0100

    update

---
+++ labor_155231_3.php
@@ -0,0 +1,7 @@
+<?php
+    $nr_indeksu = '155231';
+    $nr_grupy = '3';
+    echo 'Dominik Lewandowski '.$nr_indeksu.'grupa '.$nr_grupy.'<br /><br />';
+
+    echo 'Zastosowanie metody include(): <br />'
+?>
